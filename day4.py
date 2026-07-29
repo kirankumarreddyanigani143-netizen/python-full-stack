e13cd9ef75c47586f05c3d972e596de9eb7fc6ab
kirankumarreddyanigani143-netizen <kirankumarreddyanigani143@gmail.com>
2026-07-29 10:48:40 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,38 @@
+# class Animal:
+#     def eat(self):
+#         print("Animal eats food")
+
+# class Bird(Animal):
+#     def fly(self):
+#         print("Bird can fly")
+
+# class Parrot(Bird):
+#     def speak(self):
+#         print("Parrot can speak")
+
+# p = Parrot()
+
+# p.eat()
+# p.fly()
+# p.speak()
+
+
+# class phone:
+#     def call (self):
+#         print("calling...")
+
+# class music_player:
+#     def play_music(self):
+#         print("playing music")
+
+# class smartphone(phone, music_player):
+#     def take_photo(self):
+#         print("taking photo")
+
+# obj = smartphone()
+
+# obj.take_photo()
+# obj.play_music()
+# obj.call()
+
+
